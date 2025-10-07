9725a5221546e1e1e570adf67aa798c70aa1dab2
JACKCY ANTONY <jackcyantony7@gmail.com>
2025-10-07 15:38:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "expensetracker-23178025-60007",
+  "appId": "1:149759918379:web:9b3ecd1b14621aa74b856e",
+  "apiKey": "AIzaSyB9MZetLrEcZ2GqZKm-PBUW-BJCAmXCp-Y",
+  "authDomain": "expensetracker-23178025-60007.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "149759918379"
+};
